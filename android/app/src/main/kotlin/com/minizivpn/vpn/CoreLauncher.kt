950cd1027634bf52de7d182477e42c779538a7fa
wawuhda-gif <wawuhda@gmail.com>
2026-05-17 15:13:17 +0700

    Add VPN core launcher

---
+++ android/app/src/main/kotlin/com/minizivpn/vpn/CoreLauncher.kt
@@ -0,0 +1,12 @@
+package com.minizivpn.vpn
+
+class CoreLauncher {
+
+    fun startCore(configPath: String) {
+        println("Starting VPN core with config: $configPath")
+    }
+
+    fun stopCore() {
+        println("Stopping VPN core")
+    }
+}
