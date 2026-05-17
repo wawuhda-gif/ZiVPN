30cc0dce824863d8e4d6603c404073d2a06b971b
wawuhda-gif <wawuhda@gmail.com>
2026-05-18 01:05:27 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/minizivpn/MainActivity.kt
+++ android/app/src/main/kotlin/com/minizivpn/MainActivity.kt
@@ -1,5 +1,47 @@
-package com.example.zivpn
+package com.minizivpn
 
+import android.content.Intent
+import android.net.VpnService
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "minizivpn/vpn"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+
+            when(call.method) {
+
+                "startVPN" -> {
+
+                    val intent = VpnService.prepare(this)
+
+                    if (intent != null) {
+                        startActivityForResult(intent, 100)
+                    } else {
+                        TunnelManager.start(this)
+                    }
+
+                    result.success(true)
+                }
+
+                "stopVPN" -> {
+
+                    TunnelManager.stop(this)
+
+                    result.success(true)
+                }
+
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
