23fcc435b9e0d5e111c28e7fb4d8bf9971db3aa0
wawuhda-gif <wawuhda@gmail.com>
2026-05-17 15:13:40 +0700

    Add reconnect watchdog service

---
+++ android/app/src/main/kotlin/com/minizivpn/vpn/ReconnectWatchdog.kt
@@ -0,0 +1,12 @@
+package com.minizivpn.vpn
+
+class ReconnectWatchdog {
+
+    fun monitorConnection() {
+        println("Monitoring VPN connection...")
+    }
+
+    fun reconnectIfNeeded() {
+        println("Reconnect triggered")
+    }
+}
